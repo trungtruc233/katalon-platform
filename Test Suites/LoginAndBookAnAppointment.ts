f739a1f3d82c543821d78626b7afd872c28bd47f
ASUS <ASUS@LAPTOP-T4E3LFSV>
2023-02-22 21:39:11 +0700

    The second

--- Test Suites/LoginAndBookAnAppointment.ts
+++ Test Suites/LoginAndBookAnAppointment.ts
@@ -11,18 +11,4 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>4b197258-1b16-4101-8234-8d5a1118da25</testSuiteGuid>
-   <testCaseLink>
-      <guid>40edee4c-942a-4c06-804a-ce586c7ca5b6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC_Login</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>1971ab83-068d-4c2b-a790-d062fc2e0a7e</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/TC-MakeApointment</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
 </TestSuiteEntity>
